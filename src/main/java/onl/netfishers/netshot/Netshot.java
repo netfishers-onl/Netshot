301b27bdb0fb515b85c7a377a2f36ede2edfafa8
Alain Degreffe <eczema@ecze.com>
2018-11-02 15:16:24 +0100

    Support Snmpv3 #2

--- src/main/java/onl/netfishers/netshot/Netshot.java
+++ src/main/java/onl/netfishers/netshot/Netshot.java
@@ -51,7 +51,7 @@ import onl.netfishers.netshot.device.DeviceDriver;
 public class Netshot extends Thread {
 
 	/** Netshot version. */
-	public static final String VERSION = "0.9.0b";
+	public static final String VERSION = "0.10.0";
 
 	/** The list of configuration files to look at, in sequence. */
 	private static final String[] CONFIG_FILENAMES = new String[] {

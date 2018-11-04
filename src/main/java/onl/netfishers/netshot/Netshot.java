4c52f145f1ab8e2102c9f23f7fe78744d942d81d
Sylvain Cadilhac <sylvain.cadilhac@netfishers.onl>
2018-11-04 12:27:24 +0100

    Update libraries

--- src/main/java/onl/netfishers/netshot/Netshot.java
+++ src/main/java/onl/netfishers/netshot/Netshot.java
@@ -40,6 +40,7 @@ import ch.qos.logback.core.OutputStreamAppender;
 import ch.qos.logback.core.rolling.FixedWindowRollingPolicy;
 import ch.qos.logback.core.rolling.RollingFileAppender;
 import ch.qos.logback.core.rolling.SizeBasedTriggeringPolicy;
+import ch.qos.logback.core.util.FileSize;
 import onl.netfishers.netshot.collector.SnmpTrapReceiver;
 import onl.netfishers.netshot.collector.SyslogServer;
 import onl.netfishers.netshot.device.DeviceDriver;
@@ -191,7 +192,7 @@ public class Netshot extends Thread {
 
 				SizeBasedTriggeringPolicy<ILoggingEvent> triggeringPolicy = new 
 						SizeBasedTriggeringPolicy<ILoggingEvent>();
-				triggeringPolicy.setMaxFileSize(String.format("%dMB", logMaxSize));
+				triggeringPolicy.setMaxFileSize(new FileSize(logMaxSize * FileSize.MB_COEFFICIENT));
 				triggeringPolicy.start();
 
 				rfAppender.setRollingPolicy(fwRollingPolicy);
@@ -290,7 +291,7 @@ public class Netshot extends Thread {
 
 				SizeBasedTriggeringPolicy<ILoggingEvent> triggeringPolicy = new 
 						SizeBasedTriggeringPolicy<ILoggingEvent>();
-				triggeringPolicy.setMaxFileSize(String.format("%dMB", auditMaxSize));
+				triggeringPolicy.setMaxFileSize(new FileSize(auditMaxSize * FileSize.MB_COEFFICIENT));
 				triggeringPolicy.start();
 
 				appender.setRollingPolicy(fwRollingPolicy);

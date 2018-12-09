16f8c6a0ca232c964c5eadab2dced23be546756a
Sylvain Cadilhac <sylvain.cadilhac@netfishers.onl>
2018-12-09 12:28:27 +0100

    First version of Checkpoint Gaia driver

---
+++ src/main/resources/drivers/Checkpoint_Gaia.js
@@ -0,0 +1,309 @@
+/**
+ * Copyright 2013-2018 Sylvain Cadilhac (NetFishers)
+ * 
+ * This file is part of Netshot.
+ * 
+ * Netshot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Netshot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Netshot.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+var Info = {
+	name: "CheckpointGaia",
+	description: "Checkpoint Gaia",
+	author: "NetFishers",
+	version: "1.0"
+};
+
+var Config = {
+	"productVersion": {
+		type: "Text",
+		title: "Gaia Product Version",
+		comparable: true,
+		searchable: true,
+		checkable: true,
+		dump: {
+			preLine: "## Product version: "
+		}
+	},
+	"kernelVersion": {
+		type: "Text",
+		title: "Kernel version",
+		comparable: true,
+		searchable: true,
+		checkable: true
+	},
+	"configuration": {
+		type: "LongText",
+		title: "Configuration (clish)",
+		comparable: true,
+		searchable: true,
+		checkable: true,
+		dump: {
+			pre: "## Configuration (taken on %when%):",
+			post: "## End of configuration"
+		}
+	},
+	"cpLicense": {
+		type: "LongText",
+		title: "License",
+		comparable: true,
+		searchable: true,
+		checkable: true,
+		dump: {
+			pre: "## License status:",
+			preLine: "## ",
+			post: "## End of license"
+		}
+	}
+};
+
+var Device = {
+	"configurationSaved": {
+		type: "Binary",
+		title: "Configuration saved",
+		searchable: true
+	},
+	"osEdition": {
+		type: "Text",
+		title: "OS Edition",
+		searchable: true
+	}
+};
+
+var CLI = {
+	telnet: {
+		macros: {
+			clish: {
+				options: [ "login", "clish", "expert" ],
+				target: "clish"
+			},
+			expert: {
+				options: [ "username", "clish", "expert" ],
+				target: "expert"
+			}
+		}
+	},
+	ssh: {
+		macros: {
+			clish: {
+				options: [ "clish", "expert" ],
+				target: "clish"
+			},
+			expert: {
+				options: [ "clish", "expert" ],
+				target: "expert"
+			}
+		}
+	},
+	username: {
+		prompt: /^login: $/,
+		macros: {
+			auto: {
+				cmd: "$$NetshotUsername$$",
+				options: [ "password", "usernameAgain" ]
+			}
+		}
+	},
+	password: {
+		prompt: /^Password: $/,
+		macros: {
+			auto: {
+				cmd: "$$NetshotPassword$$",
+				options: [ "loginAgain", "clish", "expert" ]
+			}
+		}
+	},
+	loginAgain: {
+		prompt: /^login: $/,
+		fail: "Authentication failed - Telnet authentication failure."
+	},
+	clish: {
+		prompt: /^([A-Za-z\-_0-9\.]+> )$/,
+		error: /^(CLINFR0329|NMSHNM0679) .*nvalid/m,
+		pager: {
+			avoid: "set clienv rows 0",
+			match: /^-- More --$/,
+			response: " "
+		},
+		macros: {
+			expert: {
+				cmd: "expert",
+				options: [ "expert", "configure" ],
+				target: "expert"
+			},
+			save: {
+				cmd: "save config",
+				options: [ "clish" ],
+				target: "clish"
+			}
+		}
+	},
+	expert: {
+		prompt: /^(\[Expert@[A-Za-z\-_0-9\.]+:[0-9]+\]# )$/,
+		macros: {
+			clish: {
+				cmd: "clish",
+				options: [ "clish" ],
+				target: "clish"
+			},
+			clishBack: {
+				cmd: "exit",
+				options: [ "clish" ],
+				target: "clish"
+			}
+		}
+	},
+	noExpertPassword: {
+		prompt: /Expert password has not been defined/,
+		fail: "Unable to switch to expert mode - No expert password defined."
+	},
+	expertPassword: {
+		prompt: /^Enter expert password:$/,
+		macros: {
+			auto: {
+				cmd: "$$NetshotSuperPassword$$",
+				options: [ "password", "wrongPassword" ]
+			}
+		}
+	},
+	wrongPassword: {
+		prompt: /Wrong password./,
+		fail: "Invalid expert password"
+	}
+};
+
+function snapshot(cli, device, config, debug) {
+	var addMatchSet = function(e) {
+		e.matchSet = function(data, re, field, defaultValue) {
+			var r = data.match(re);
+			if (r) {
+				e.set(field, r[1]);
+			}
+			else if (defaultValue) {
+				e.set(field, defaultValue);
+			}
+		} 
+	}
+	addMatchSet(config);
+	addMatchSet(device);
+	
+	
+	cli.macro("clish");
+	var showConfig = cli.command("show configuration");
+	showConfig = showConfig.replace(/^(# Exported by .*) on .*/mg, "$1");
+	config.set("configuration", showConfig);
+	
+	var configState = cli.command("show config-state");
+	device.set("configurationSaved", !!configState.match(/^saved/));
+	
+	device.matchSet(showConfig, /^set hostname (.+)/m, "name");
+	device.matchSet(showConfig, /^set snmp contact "(.+)"/m, "contact");
+	device.matchSet(showConfig, /^set snmp location "(.+)"/m, "location");
+	
+	var showVersion = cli.command("show version all");
+	config.matchSet(showVersion, /^Product version (.+)/m, "productVersion");
+	config.matchSet(showVersion, /^OS kernel version (.+)/m, "kernelVersion");
+	device.matchSet(showVersion, /^OS edition (.+)/m, "osEdition");
+	
+	var version = showVersion.match(/^Product version Check Point Gaia (.+)/m);
+	if (version) {
+		device.set("softwareVersion", version[1]);
+	}
+	
+	var license = cli.command("cplic print");
+	config.set("cpLicense", license);
+
+	device.set("networkClass", "FIREWALL");
+	
+	var showInterfaces = cli.command("show interfaces all");
+	var interfaces = cli.findSections(showConfig, /^Interface (.+)/m);
+	for (var i in interfaces) {
+		var networkInterface = {
+			name: interfaces[i].match[1],
+			ip: []
+		};
+		var description = interfaces[i].config.match(/^ *comments \{(.+)\}|(.+)$/m);
+		if (description) {
+			networkInterface.description = description[1] || description[2];
+		}
+		var ipv4 = interfaces[i].config.match(/^ *ipv4-address (\d+\.\d+\.\d+\.\d+)\/(\d+)/m);
+		if (ipv4) {
+			networkInterface.ip.push({
+				ip: ipv4[1],
+				mask: parseInt(ipv4[2]),
+				usage: "PRIMARY"
+			});
+			var vrrpPattern = /^set vrrp interface (.+) monitored-circuit vrid (\d+) backup-address (\d+\.\d+\.\d+\.\d+)/mg;
+			while (match = vrrpPattern.exec(config)) {
+				if (match[1] == networkInterface.name) {
+					networkInterface.ip.push({
+						ip: match[3],
+						mask: parseInt(ipv4[2]),
+						usage: "VRRP"
+					});
+				}
+			}
+		}
+		var ipv6 = interfaces[i].config.match(/^ *ipv6-address ([A-Fa-f0-9:]+)\/(\d+)/m);
+		if (ipv6) {
+			networkInterface.ip.push({
+				ip: match[1],
+				mask: parseInt(match[2]),
+				usage: "PRIMARY"
+			});
+		}
+		var macAddress = interfaces[i].config.match(/^ *mac-addr ([0-9A-Za-z:]+)/m);
+		if (macAddress) {
+			networkInterface.mac = macAddress[1];
+		}
+		if (interfaces[i].config.match(/^ state off/m)) {
+			networkInterface.enabled = false;
+		}
+		device.add("networkInterface", networkInterface);
+	}
+	
+	var showAsset = cli.command("show asset all");
+	device.matchSet(showAsset, /^Model (.+)/m, "family", "Check Point");
+	
+	var parts = ["Motherboard", "Chassis"];
+	for (var p in parts) {
+		var part = parts[p];
+		var partNumber = showAsset.match(new RegExp("^" + part + " Assembly Part Number: (.+)", "m"));
+		var serialNumber = showAsset.match(new RegExp("^" + part + " Serial Number: (.+)", "m"));
+		if (partNumber && serialNumber) {
+			device.add("module", {
+				slot: part,
+				partNumber: partNumber[1],
+				serialNumber: serialNumber[1]
+			});
+		}
+		if (serialNumber) {
+			device.set("serialNumber", serialNumber[1]);
+		}
+	}
+};
+
+function analyzeSyslog(message) {
+	if (message.match(/%SYS\-5\-CONFIG_I: Configured from (.*) by (.*)/)) {
+		return true;
+	}
+	return false;
+}
+
+function analyzeTrap(trap, debug) {
+	return trap["1.3.6.1.6.3.1.1.4.1.0"] == "1.3.6.1.4.1.2620.1.3000.10.1.1";
+}
+
+function snmpAutoDiscover(sysObjectID, sysDesc) {
+	return sysObjectID.substring(0, 17) == "1.3.6.1.4.1.2620.";
+}

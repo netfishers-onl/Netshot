7404aacbc7c7447e0c04d4a4afeb7032e5a9f1cb
Sylvain Cadilhac <sylvain.cadilhac@netfishers.onl>
2021-11-21 23:31:41 +0100

    Adjust Python file system access filter

--- src/main/java/onl/netfishers/netshot/compliance/rules/PythonRule.java
+++ src/main/java/onl/netfishers/netshot/compliance/rules/PythonRule.java
@@ -28,7 +28,6 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;

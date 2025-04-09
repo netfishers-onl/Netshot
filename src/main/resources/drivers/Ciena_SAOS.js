66a2ee9f1308e27a44c3fac19e34d48497685b27
Sylvain Cadilhac <sylvain.cadilhac@netshot.net>
2025-04-09 14:34:40 +0200

    fix(driver): :bug: Ciena SAOS: fill serial number

--- src/main/resources/drivers/Ciena_SAOS.js
+++ src/main/resources/drivers/Ciena_SAOS.js
@@ -1,18 +1,18 @@
 /**
  * Copyright 2013-2025 Netshot
- * 
+ *
  * This file is part of Netshot project.
- * 
+ *
  * Netshot is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Netshot is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Netshot.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -21,7 +21,7 @@ var Info = {
 	name: "CienaSAOS",
 	description: "Ciena SAOS",
 	author: "Netshot Team",
-	version: "1.0.1"
+	version: "1.1"
 };
 
 var Config = {
@@ -172,11 +172,13 @@ function snapshot(cli, device, config) {
 	if (chassisMacAddress) {
 		device.set("chassisMacAddress", chassisMacAddress);
 	}
+	var serialNumber = chassisShowDevice["Serial Number"] || "Unknown";
 	device.add("module", {
 		slot: "Chassis",
 		partNumber: chassisShowDevice["Part Number/Revision"] || "Unknown",
-		serialNumber: chassisShowDevice["Serial Number"] || "Unknown",
+		serialNumber,
 	});
+	device.set("serialNumber", serialNumber);
 
 	var chassisShowCapa = parseTable("chassis show capabilities");
 	var platformName = chassisShowCapa["Platform Name"];

2afb02b37caf24bf24aa7b4b060ef572e0c20a08
Sylvain Cadilhac <sylvain.cadilhac@netshot.net>
2026-07-18 02:29:53 +0200

    build(web): :construction_worker: update eslint config

--- src/main/resources/webui/eslint.config.ts
+++ src/main/resources/webui/eslint.config.ts
@@ -1,31 +1,27 @@
 import js from "@eslint/js"
 import eslintConfigPrettier from "eslint-config-prettier/flat"
-import pluginReact from "eslint-plugin-react"
-import { defineConfig } from "eslint/config"
 import globals from "globals"
+import eslintReact from "@eslint-react/eslint-plugin";
+import eslintJs from "@eslint/js";
+import { defineConfig } from "eslint/config"
 import tseslint from "typescript-eslint"
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    plugins: { js },
-    extends: ["js/recommended"],
+    files: ["**/*.ts", "**/*.tsx"],
+    extends: [
+      eslintJs.configs.recommended,
+      tseslint.configs.recommended,
+      eslintReact.configs["recommended-typescript"],
+    ],
+    // Configure language/parsing options
     languageOptions: {
-      globals: globals.browser,
-    },
-  },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  eslintConfigPrettier,
-  {
-    settings: {
-      react: {
-        version: "detect",
+      // Use TypeScript ESLint parser for TypeScript files
+      parser: tseslint.parser,
+      parserOptions: {
+        // Enable project service for better TypeScript integration
+        projectService: true,
       },
-    },
-    rules: {
-      "react/react-in-jsx-scope": "off",
-      "react/display-name": "off",
     },
   },
 ])
